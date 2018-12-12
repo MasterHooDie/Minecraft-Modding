2d9dda5c362b19fe377ce00a196dee2219624ac3
MasterHooDie <masterhoodie@gmail.com>
2018-12-12 16:30:38 -0600

    After Episode 5, Added inventory and GUI to Fast Furnace

--- src/main/java/hoodie/mymod/furnace/BlockFastFurnace.java
+++ src/main/java/hoodie/mymod/furnace/BlockFastFurnace.java
@@ -2,14 +2,19 @@ package hoodie.mymod.furnace;
 
 import hoodie.mymod.MyMod;
 import net.minecraft.block.Block;
+import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.PropertyDirection;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
@@ -17,7 +22,9 @@ import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
-public class BlockFastFurnace extends Block {
+import javax.annotation.Nullable;
+
+public class BlockFastFurnace extends Block implements ITileEntityProvider {
 
     public static final PropertyDirection FACING = PropertyDirection.create("facing");
 
@@ -35,6 +42,26 @@ public class BlockFastFurnace extends Block {
         setTranslationKey(MyMod.MODID + ".fast_furnace");
 
         setDefaultState(blockState.getBaseState().withProperty(FACING, EnumFacing.NORTH));
+    }
+
+    @Nullable
+    @Override
+    public TileEntity createNewTileEntity(World worldIn, int i) {
+        return new TileFastFurnace();
+    }
+
+    @Override
+    public boolean onBlockActivated(World world, BlockPos pos, IBlockState state, EntityPlayer player, EnumHand hand, EnumFacing side, float hitX, float hitY, float hitZ) {
+        // Only execute on the server
+        if (world.isRemote) {
+            return true;
+        }
+        TileEntity te = world.getTileEntity(pos);
+        if (!(te instanceof TileFastFurnace)) {
+            return false;
+        }
+        player.openGui(MyMod.instance, 0, world, pos.getX(), pos.getY(), pos.getZ());
+        return true;
     }
 
     @SideOnly(Side.CLIENT)
