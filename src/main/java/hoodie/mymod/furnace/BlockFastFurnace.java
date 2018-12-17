31b07947652140070de8d5e9cce423623e1baf2d
MasterHooDie <masterhoodie@gmail.com>
2018-12-17 12:47:09 -0600

    After Episode 9, Made Fast furnace front panel animated

--- src/main/java/hoodie/mymod/furnace/BlockFastFurnace.java
+++ src/main/java/hoodie/mymod/furnace/BlockFastFurnace.java
@@ -5,6 +5,7 @@ import net.minecraft.block.Block;
 import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.PropertyDirection;
+import net.minecraft.block.properties.PropertyEnum;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
@@ -17,7 +18,10 @@ import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.ChunkCache;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+import net.minecraft.world.chunk.Chunk;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -27,7 +31,7 @@ import javax.annotation.Nullable;
 public class BlockFastFurnace extends Block implements ITileEntityProvider {
 
     public static final PropertyDirection FACING = PropertyDirection.create("facing");
-
+    public static final PropertyEnum<FurnaceState> STATE = PropertyEnum.<FurnaceState>create("state", FurnaceState.class);
 
 
     public static final ResourceLocation FAST_FURNACE = new ResourceLocation(MyMod.MODID,"fast_furnace");
@@ -75,18 +79,27 @@ public class BlockFastFurnace extends Block implements ITileEntityProvider {
     }
 
     @Override
+    public IBlockState getActualState(IBlockState state, IBlockAccess world, BlockPos pos) {
+        TileEntity te = world instanceof ChunkCache ? ((ChunkCache)world).getTileEntity(pos, Chunk.EnumCreateEntityType.CHECK) : world.getTileEntity(pos);
+        if (te instanceof TileFastFurnace) {
+            return state.withProperty(STATE, ((TileFastFurnace) te).getState());
+        }
+        return super.getActualState(state, world, pos);
+    }
+
+    @Override
     public IBlockState getStateFromMeta(int meta) {
         //return this.getDefaultState().withProperty(FACING, EnumFacing.getFront(meta, 7));
         return this.getDefaultState().withProperty(FACING, EnumFacing.NORTH);
     }
 
     @Override
-    public int getMetaFromState(IBlockState state) {
-        return state.getValue(FACING).getIndex();
+    protected BlockStateContainer createBlockState() {
+        return new BlockStateContainer(this, FACING, STATE);
     }
 
     @Override
-    protected BlockStateContainer createBlockState() {
-        return new BlockStateContainer(this, FACING);
+    public int getMetaFromState(IBlockState state) {
+        return state.getValue(FACING).getIndex();
     }
 }
