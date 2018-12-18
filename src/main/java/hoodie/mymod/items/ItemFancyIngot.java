5e23926bf766ddee4b8a28209d5b3b3f7e4ac21d
MasterHooDie <masterhoodie@gmail.com>
2018-12-18 13:21:03 -0600

    After Episode 12, added ingot and smelting

---
+++ src/main/java/hoodie/mymod/items/ItemFancyIngot.java
@@ -0,0 +1,23 @@
+package hoodie.mymod.items;
+
+import hoodie.mymod.MyMod;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.item.Item;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+public class ItemFancyIngot extends Item {
+
+    public ItemFancyIngot() {
+        setTranslationKey(MyMod.MODID + ".fancy_ingot");
+        setRegistryName(new ResourceLocation(MyMod.MODID, "fancy_ingot"));
+        setCreativeTab(MyMod.creativeTab);
+    }
+
+    @SideOnly(Side.CLIENT)
+    public void initModel() {
+        ModelLoader.setCustomModelResourceLocation(this, 0, new ModelResourceLocation(getRegistryName(), "inventory"));
+    }
+}
--- src/main/java/hoodie/mymod/proxy/CommonProxy.java
+++ src/main/java/hoodie/mymod/proxy/CommonProxy.java
@@ -2,9 +2,11 @@ package hoodie.mymod.proxy;
 
 import com.google.common.util.concurrent.ListenableFuture;
 import hoodie.mymod.ModBlocks;
+import hoodie.mymod.ModItems;
 import hoodie.mymod.MyMod;
 import hoodie.mymod.furnace.BlockFastFurnace;
 import hoodie.mymod.furnace.TileFastFurnace;
+import hoodie.mymod.items.ItemFancyIngot;
 import hoodie.mymod.network.Messages;
 import hoodie.mymod.worldgen.BlockFancyOre;
 import hoodie.mymod.worldgen.OreGenerator;
@@ -13,6 +15,7 @@ import net.minecraft.block.Block;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -22,6 +25,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.network.NetworkRegistry;
 import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.oredict.OreDictionary;
 
 @Mod.EventBusSubscriber
 public class CommonProxy {
@@ -37,6 +41,8 @@ public class CommonProxy {
     }
 
     public void postInit(FMLPostInitializationEvent e) {
+        GameRegistry.addSmelting(ModBlocks.blockFancyOre, new ItemStack(ModItems.itemFancyIngot, 1), 0.5f);
+        OreDictionary.registerOre("oreFancy", ModBlocks.blockFancyOre);
     }
 
     @SubscribeEvent
@@ -57,6 +63,7 @@ public class CommonProxy {
                 }
                         .setHasSubtypes(true)
                         .setRegistryName(BlockFancyOre.FANCY_ORE));
+        event.getRegistry().register(new ItemFancyIngot());
     }
 
     public ListenableFuture<Object> addScheduledTaskClient(Runnable runnableToSchedule) {
