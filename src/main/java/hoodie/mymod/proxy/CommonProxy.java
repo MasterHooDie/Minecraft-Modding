c5addfa830e5ed4478ebf149b85fd35e2b3b9e70
MasterHooDie <masterhoodie@gmail.com>
2018-12-18 09:57:34 -0600

    During Episode 12, Added fancy ore blocks

--- src/main/java/hoodie/mymod/proxy/CommonProxy.java
+++ src/main/java/hoodie/mymod/proxy/CommonProxy.java
@@ -6,6 +6,7 @@ import hoodie.mymod.MyMod;
 import hoodie.mymod.furnace.BlockFastFurnace;
 import hoodie.mymod.furnace.TileFastFurnace;
 import hoodie.mymod.network.Messages;
+import hoodie.mymod.worldgen.BlockFancyOre;
 import net.minecraft.block.Block;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
@@ -36,11 +37,20 @@ public class CommonProxy {
     public static void registerBlocks(RegistryEvent.Register<Block> event) {
         event.getRegistry().register(new BlockFastFurnace());
         GameRegistry.registerTileEntity(TileFastFurnace.class, MyMod.MODID + "_fast_furnace");
+        event.getRegistry().register(new BlockFancyOre());
     }
 
     @SubscribeEvent
     public static void registerItems(RegistryEvent.Register<Item> event) {
         event.getRegistry().register(new ItemBlock(ModBlocks.blockFastFurnace).setRegistryName(ModBlocks.blockFastFurnace.getRegistryName()));
+        event.getRegistry().register(new ItemBlock(ModBlocks.blockFancyOre) {
+                    @Override
+                    public int getMetadata(int damage) {
+                        return damage;
+                    }
+                }
+                        .setHasSubtypes(true)
+                        .setRegistryName(BlockFancyOre.FANCY_ORE));
     }
 
     public ListenableFuture<Object> addScheduledTaskClient(Runnable runnableToSchedule) {
